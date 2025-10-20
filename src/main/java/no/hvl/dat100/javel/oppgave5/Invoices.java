ba77c19d0c0fe13ea95aa769bc62db154d4e1a6e
Alissa-Ruya <rs.aksel.7@gmail.com>
2025-10-20 16:18:48 +0200

    update 5 Invoices

--- src/main/java/no/hvl/dat100/javel/oppgave5/Invoices.java
+++ src/main/java/no/hvl/dat100/javel/oppgave5/Invoices.java
@@ -4,7 +4,25 @@ public class Invoices {
 
     public static void processInvoices(Invoice[] invoices) { // alissa
 
-        // TODO
+        if(invoices ==null) {
+            throw new IllegalArgumentException("invoices array can't be null");
+        }
+
+        int processed = 0;
+
+        for (int i = 0; i< invoices.length; i++) {
+            Invoice inv = invoices[i];
+            if(inv == null) {
+                continue;
+            }
+            inv.computeAmount();
+
+            inv.printInvoice();
+
+            processed++;
+        }
+
+        System.out.println("==Invoices processed: " + processed + "/" + invoices.length + "==");
 
     }
 }
